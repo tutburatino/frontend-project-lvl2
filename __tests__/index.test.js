f53d07117fd639bccc5b84ab2112de8daef39fbe
mike <tutburatino@gmail.com>
2020-02-04 01:18:05 +0300

    added feature support of INI files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,10 +4,15 @@ import genDiff from '../src/';
 
 const result = fs.readFileSync('__tests__/__fixtures__/result.txt', 'utf-8');
 const result2 = fs.readFileSync('__tests__/__fixtures__/result2.txt', 'utf-8');
+
 const json1path = `${__dirname}/__tests__/__fixtures__/before.json`;
 const json2path =  `${__dirname}/__tests__/__fixtures__/after.json`;
+
 const yaml1path = `${__dirname}/__tests__/__fixtures__/before.yml`;
 const yaml2path =  `${__dirname}/__tests__/__fixtures__/after.yml`;
+
+const ini1path = `${__dirname}/__tests__/__fixtures__/before.ini`;
+const ini2path =  `${__dirname}/__tests__/__fixtures__/after.ini`;
 
 
 test('Compared two JSON files and in two variants of sequence.\
@@ -26,5 +31,15 @@ test('Compared two YAML files and in two variants of sequence.\
   expect(actual).toBe(result);
 
   const actual2 = genDiff(yaml2path, yaml1path);
+  expect(actual2).toBe(result2);
+});
+
+
+test('Compared two INI files and in two variants of sequence.\
+    \nPaths are absolutely. Should return the difference.', () => {
+  const actual = genDiff(ini1path, ini2path);
+  expect(actual).toBe(result);
+
+  const actual2 = genDiff(ini2path, ini1path);
   expect(actual2).toBe(result2);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,12 +1,13 @@
 import yaml from 'js-yaml';
 import fs from 'fs';
 import path from 'path';
-
+import ini from 'ini';
 
 const parsers = {
   yml: data => yaml.safeLoad(data),
   json: data => JSON.parse(data),
   yaml: data => yaml.safeLoad(data),
+  ini: data => ini.parse(data),
 };
 
 export default (pathToFile) => {
