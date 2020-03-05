bad3093e7218fe9576d233f219de4640b5d86166
mike <tutburatino@gmail.com>
2020-03-05 18:36:11 +0300

    corr descr of tests and paths of fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,80 +1,52 @@
+import path from 'path';
 import fs from 'fs';
 import genDiff from '../src';
 
 
-const result = fs.readFileSync(`${__dirname}/__fixtures__/result.txt`, 'utf-8');
-const result2 = fs.readFileSync(`${__dirname}/__fixtures__/result2.txt`, 'utf-8');
+const getFixturePath = filename => path.join(__dirname, '__fixtures__', filename);
+const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 
-// eslint-disable-next-line no-multi-str
-test('# 1 Compare 2 JSON files and in 2 variants of sequence.\
-    \nPaths are absolutely. Should return the difference.', () => {
-  const json1path = `${__dirname}/__fixtures__/before.json`;
-  const json2path = `${__dirname}/__fixtures__/after.json`;
-  const actual = genDiff(json1path, json2path);
-  expect(actual).toBe(result);
+let beforePath;
+let afterPath;
 
-  const actual2 = genDiff(json2path, json1path);
-  expect(actual2).toBe(result2);
+beforeAll(() => {
+  beforePath = `${__dirname}/__fixtures__/beforeTree.json`;
+  afterPath = `${__dirname}/__fixtures__/afterTree.json`;
 });
 
 
-// eslint-disable-next-line no-multi-str
-test('# 2 Compare 2 YAML files and in 2 variants of sequence.\
-    \nPaths are absolutely. Should return the difference.', () => {
-  const yaml1path = `${__dirname}/__fixtures__/before.yml`;
-  const yaml2path = `${__dirname}/__fixtures__/after.yml`;
-
-  const actual = genDiff(yaml1path, yaml2path);
-  expect(actual).toBe(result);
-
-  const actual2 = genDiff(yaml2path, yaml1path);
-  expect(actual2).toBe(result2);
+test('JSON', () => {
+  const actual = genDiff(getFixturePath('before.json'), getFixturePath('after.json'));
+  expect(actual).toBe(readFile('result.txt'));
 });
 
 
-// eslint-disable-next-line no-multi-str
-test('# 3 Compare 2 INI files and in 2 variants of sequence.\
-    \nPaths are absolutely. Should return the difference.', () => {
-  const ini1path = `${__dirname}/__fixtures__/before.ini`;
-  const ini2path = `${__dirname}/__fixtures__/after.ini`;
-
-  const actual = genDiff(ini1path, ini2path);
-  expect(actual).toBe(result);
-
-  const actual2 = genDiff(ini2path, ini1path);
-  expect(actual2).toBe(result2);
+test('YAML', () => {
+  const actual = genDiff(getFixturePath('before.yml'), getFixturePath('after.yml'));
+  expect(actual).toBe(readFile('result.txt'));
 });
 
-// eslint-disable-next-line no-multi-str
-test('# 4 Compare 2 JSON tree files and in 2 variants of sequence.\
-    \nPaths are absolutely. Should return the difference.', () => {
-  const jsonTree1path = `${__dirname}/__fixtures__/beforeTree.json`;
-  const jsonTree2path = `${__dirname}/__fixtures__/afterTree.json`;
-  const result4Trees = fs.readFileSync(`${__dirname}/__fixtures__/result4Trees.txt`, 'utf-8');
 
-  const actual = genDiff(jsonTree1path, jsonTree2path);
-  expect(actual).toBe(result4Trees);
+test('INI', () => {
+  const actual = genDiff(getFixturePath('before.ini'), getFixturePath('after.ini'));
+  expect(actual).toBe(readFile('result.txt'));
 });
 
-// eslint-disable-next-line no-multi-str
-test('# 5 Plain format. Compare 2 JSON tree files and in 2 variants of sequence.\
-    \nPaths are absolutely. Should return the difference.', () => {
-  const jsonTree1path = `${__dirname}/__fixtures__/beforeTree.json`;
-  const jsonTree2path = `${__dirname}/__fixtures__/afterTree.json`;
-  const plainResult4Trees = fs.readFileSync(`${__dirname}/__fixtures__/plainResult4Trees.txt`, 'utf-8');
 
-  const actual = genDiff(jsonTree1path, jsonTree2path, 'plain');
-  expect(actual).toBe(plainResult4Trees);
+test('Nested JSON', () => {
+  const actual = genDiff(beforePath, afterPath);
+  expect(actual).toBe(readFile('result4Trees.txt'));
 });
 
-// eslint-disable-next-line no-multi-str
-test('# 6 JSON format. Compare 2 JSON tree files and in 2 variants of sequence.\
-    \nPaths are absolutely. Should return the difference.', () => {
-  const beforesPath = `${__dirname}/__fixtures__/beforeTree.json`;
-  const aftersPath = `${__dirname}/__fixtures__/afterTree.json`;
-  const resultFromFile = fs.readFileSync(`${__dirname}/__fixtures__/result4Trees.json`, 'utf-8');
 
-  const actual = genDiff(beforesPath, aftersPath, 'JSON');
-  expect(actual).toBe(resultFromFile);
+test('Plain format output', () => {
+  const actual = genDiff(beforePath, afterPath, 'plain');
+  expect(actual).toBe(readFile('plainFormat.txt'));
+});
+
+
+test('JSON format output', () => {
+  const actual = genDiff(beforePath, afterPath, 'JSON');
+  expect(actual).toBe(readFile('jsonFormat.json'));
 });
