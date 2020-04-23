a127c7d0b3670c3b832c902d4a22dcf6a4262316
mike <tutburatino@gmail.com>
2020-04-23 17:06:19 +0300

    add test for call genDiff without last argument format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,14 +8,20 @@ const readFile = filename => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 
 test.each([
-  ['yaml1.yml', 'yaml2.yml', 'result.txt', 'tree'],
   ['ini1.ini', 'ini2.ini', 'result.txt', 'tree'],
   ['json1.json', 'json2.json', 'format-tree.txt', 'tree'],
   ['json1.json', 'json2.json', 'format-plain.txt', 'plain'],
   ['json1.json', 'json2.json', 'format-json.json', 'json'],
-])('genDiff(%s, %s)', (file1, file2, result, format) => {
+  ['yaml1.yml', 'yaml2.yml', 'result.txt', 'tree'],
+])('genDiff(%s, %s, %s, %s)', (file1, file2, result, format) => {
   const path1 = getFixturePath(file1);
   const path2 = getFixturePath(file2);
   const expected = readFile(result);
   expect(genDiff(path1, path2, format)).toBe(expected);
+});
+
+test("genDiff('json1.json', 'json2.json') should return tree", () => {
+  const actual = genDiff(getFixturePath('json1.json'), getFixturePath('json2.json'));
+  const expected = readFile('format-tree.txt');
+  expect(actual).toBe(expected);
 });
